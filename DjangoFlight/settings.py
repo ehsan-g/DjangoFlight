6675bf5c6b4a430a69ec2155d210dbe607db129a
ehsan-g <Mona@ahmad.com>
2020-02-24 16:33:43 +0330

    fixed .travis.yml but tests not passed

--- DjangoFlight/settings.py
+++ DjangoFlight/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'djangoflightproject',
-        'USER': 'postgresflights',
+        'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '',
